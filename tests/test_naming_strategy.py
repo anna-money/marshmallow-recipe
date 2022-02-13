d746de14dc04e74631a2548caef34318ec60061e
Yury Pliner <yury.pliner@gmail.com>
2022-02-13 15:39:56 +0000

    Customize capital camel case and add camel case

--- tests/test_naming_strategy.py
+++ tests/test_naming_strategy.py
@@ -1,6 +1,6 @@
 import pytest
 
-import marshmallow_recipe
+import marshmallow_recipe as mr
 
 
 @pytest.mark.parametrize(
@@ -10,7 +10,25 @@ import marshmallow_recipe
         ("hello_world", "HelloWorld"),
         ("answer_is_42", "AnswerIs42"),
         ("hello__world", "HelloWorld"),
+        ("foo_bar", "FooBAR"),
     ],
 )
 def test_capital_camel_case(name: str, expected: str) -> None:
-    assert expected == marshmallow_recipe.capital_camel_case(name)
+    capital_camel_case = mr.capital_camel_case_factory(capitalize_words={"bar"})
+    assert expected == capital_camel_case(name)
+
+
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        ("hello", "hello"),
+        ("hello_world", "helloWorld"),
+        ("answer_is_42", "answerIs42"),
+        ("hello__world", "helloWorld"),
+        ("foo_bar", "fooBAR"),
+        ("bar_foo", "barFoo"),
+    ],
+)
+def test_camel_case(name: str, expected: str) -> None:
+    camel_case = mr.camel_case_factory(capitalize_words={"bar"})
+    assert expected == camel_case(name)
