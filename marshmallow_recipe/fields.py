d9b0f2173c16775950395fd474ac63e44745e878
Yury Pliner <yury.pliner@gmail.com>
2022-02-14 21:47:42 +0000

    Inline unused parameters

--- marshmallow_recipe/fields.py
+++ marshmallow_recipe/fields.py
@@ -74,20 +74,18 @@ def decimal_field(
     required: bool,
     name: str,
     default: Any | None | MissingType,
-    places: int = 2,
-    as_string: bool = True,
     **_: Any,
 ) -> m.fields.Field:
     if required:
         if default is not MISSING:
             raise ValueError("Default values is not supported for required fields")
-        return m.fields.Decimal(dump_to=name, load_from=name, as_string=as_string, places=places, required=True)
+        return m.fields.Decimal(dump_to=name, load_from=name, as_string=True, places=2, required=True)
 
     return m.fields.Decimal(
         dump_to=name,
         load_from=name,
-        as_string=as_string,
-        places=places,
+        as_string=True,
+        places=2,
         allow_none=True,
         missing=None if default is MISSING else default,
         default=None if default is MISSING else default,
