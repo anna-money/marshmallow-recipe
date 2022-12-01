c369c79602db9735257438254fcab5391bba614d
Yury Pliner <yury.pliner@gmail.com>
2022-12-01 13:03:56 +0000

    Ignore unknown fields for marshmallow2

---
+++ tests/test_unknown_fields.py
@@ -0,0 +1,12 @@
+import dataclasses
+import marshmallow_recipe as mr
+
+
+def test_unknown_fields_should_be_excluded() -> None:
+    @dataclasses.dataclass
+    class Example:
+        field_1: str = dataclasses.field(metadata=mr.metadata(name="field_2"))
+
+    expected = mr.load(Example, {"field_1": "bad", "field_2": "good"})
+    assert expected == Example(field_1="good")
+
