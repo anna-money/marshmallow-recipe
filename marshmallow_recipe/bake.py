a7a1a863f9c8881f6400dd6a05e2c1756de18c0a
TimurAM <51739990+timuram@users.noreply.github.com>
2022-12-20 15:12:02 +0300

    Non-optional fields with default value might not present in data (#76)
    * make use of dataclass defaults
    
    * cover case of core dataclass behaviour change
    
    * Fix review comments
    
    Co-authored-by: Yury Pliner <yury.pliner@gmail.com>

--- marshmallow_recipe/bake.py
+++ marshmallow_recipe/bake.py
@@ -82,34 +82,30 @@ def get_field_for(
 
     type = _substitute_any_to_open_generic(type)
 
-    if typing_inspect.is_union_type(type):
-        type_args = list(set(typing_inspect.get_args(type, True)))
-        if types.NoneType not in type_args or len(type_args) != 2:
-            raise ValueError(f"Unsupported {type=}")
+    if underlying_type_from_optional := _try_get_underlying_type_from_optional(type):
         required = False
-        type = next(type_arg for type_arg in type_args if type_arg is not types.NoneType)  # noqa
-    # to support new union syntax
-    elif isinstance(type, types.UnionType):
-        type_args = list(set(type.__args__))
-        if types.NoneType not in type_args or len(type_args) != 2:
-            raise ValueError(f"Unsupported {type=}")
+        allow_none = True
+        type = underlying_type_from_optional
+    elif metadata.get("default", dataclasses.MISSING) is not dataclasses.MISSING:
         required = False
-        type = next(type_arg for type_arg in type_args if type_arg is not types.NoneType)  # noqa
+        allow_none = False
     else:
         required = True
+        allow_none = False
 
     field_factory = _SIMPLE_TYPE_FIELD_FACTORIES.get(type)
     if field_factory:
         typed_field_factory = cast(_FieldFactory, field_factory)
-        return typed_field_factory(required=required, **metadata)
+        return typed_field_factory(required=required, allow_none=allow_none, **metadata)
 
     if inspect.isclass(type) and issubclass(type, enum.Enum):
-        return enum_field(enum_type=type, required=required, **metadata)
+        return enum_field(enum_type=type, required=required, allow_none=allow_none, **metadata)
 
     if dataclasses.is_dataclass(type):
         return nested_field(
             bake_schema(type, naming_case=naming_case),
             required=required,
+            allow_none=allow_none,
             **metadata,
         )
 
@@ -119,11 +115,13 @@ def get_field_for(
             return list_field(
                 get_field_for(arguments[0], metadata={}, naming_case=naming_case),
                 required=required,
+                allow_none=allow_none,
                 **metadata,
             )
         if origin in (dict, Dict) and arguments[0] is str and arguments[1] is Any:
             return dict_field(
                 required=required,
+                allow_none=allow_none,
                 **metadata,
             )
 
@@ -194,6 +192,7 @@ class _FieldFactory(Protocol):
         self,
         *,
         required: bool,
+        allow_none: bool,
         name: str,
         default: Any,
         **kwargs: Any,
@@ -225,3 +224,20 @@ def _substitute_any_to_open_generic(type: type) -> type:
     if type is dict:
         return dict[Any, Any]
     return type
+
+
+def _try_get_underlying_type_from_optional(type: type) -> type | None:
+    if typing_inspect.is_union_type(type):
+        type_args = list(set(typing_inspect.get_args(type, True)))
+        if types.NoneType not in type_args or len(type_args) != 2:
+            raise ValueError(f"Unsupported {type=}")
+        return next(type_arg for type_arg in type_args if type_arg is not types.NoneType)  # noqa
+
+    # to support new union syntax
+    if isinstance(type, types.UnionType):
+        type_args = list(set(type.__args__))
+        if types.NoneType not in type_args or len(type_args) != 2:
+            raise ValueError(f"Unsupported {type=}")
+        return next(type_arg for type_arg in type_args if type_arg is not types.NoneType)  # noqa
+
+    return None
--- tests/test_missing.py
+++ tests/test_missing.py
@@ -322,3 +322,20 @@ def test_nested_with_missing() -> None:
     dumped = mr.dump(filled_data)
     loaded = mr.load(TypeWithNested, dumped)
     assert loaded == filled_data
+
+
+def test_meaningful_default() -> None:
+    @dataclasses.dataclass(frozen=True, slots=True, kw_only=True)
+    class WithDefault:
+        field_str: str
+        field_bool: bool
+        field_int: int
+        field_str_default: str = "default"
+        field_bool_default: bool = False
+        field_int_default: int = 1
+
+    loaded = mr.load(WithDefault, {"field_str": "str", "field_bool": True, "field_int": 0})
+    assert loaded == WithDefault(field_str="str", field_bool=True, field_int=0)
+    assert loaded.field_int_default == 1
+    assert loaded.field_bool_default is False
+    assert loaded.field_str_default == "default"
