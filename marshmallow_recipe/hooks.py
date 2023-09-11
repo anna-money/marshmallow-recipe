a4a141b174c73d6e3e88ad75cea4e95ab657aa68
Iurii Pliner <yury.pliner@gmail.com>
2023-09-11 09:28:17 +0100

    Small renames

--- marshmallow_recipe/hooks.py
+++ marshmallow_recipe/hooks.py
@@ -2,27 +2,27 @@ import collections.abc
 import inspect
 from typing import Any, Type
 
-_PRE_LOAD = "__marshmallow_recipe_pre_load_"
-_PRE_LOAD_CLASS_ = "__marshmallow_recipe_pre_load_class_"
+_PRE_LOAD_KEY = "__marshmallow_recipe_pre_load__"
+_PRE_LOAD_CLASS_KEY = "__marshmallow_recipe_pre_load_class__"
 
 
 def pre_load(fn: collections.abc.Callable[..., Any]) -> collections.abc.Callable[..., Any]:
-    setattr(fn, _PRE_LOAD, True)
+    setattr(fn, _PRE_LOAD_KEY, True)
     return fn
 
 
 def get_pre_loads(cls: Type) -> list[collections.abc.Callable[..., Any]]:
     result = []
     for _, method in inspect.getmembers(cls):
-        if hasattr(method, _PRE_LOAD):
+        if hasattr(method, _PRE_LOAD_KEY):
             result.append(method)
-    for fn in getattr(cls, _PRE_LOAD_CLASS_, []):
+    for fn in getattr(cls, _PRE_LOAD_CLASS_KEY, []):
         result.append(fn)
     return result
 
 
 def add_pre_load(cls: Type, fn: collections.abc.Callable[[dict[str, Any]], dict[str, Any]]) -> None:
-    if not hasattr(cls, _PRE_LOAD_CLASS_):
-        setattr(cls, _PRE_LOAD_CLASS_, [])
-    pre_loads: list = getattr(cls, _PRE_LOAD_CLASS_)
+    if not hasattr(cls, _PRE_LOAD_CLASS_KEY):
+        setattr(cls, _PRE_LOAD_CLASS_KEY, [])
+    pre_loads: list = getattr(cls, _PRE_LOAD_CLASS_KEY)
     pre_loads.append(fn)
