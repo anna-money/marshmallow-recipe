e7200960ac6cae0499eb707216428940d20d7da8
Iurii Pliner <yury.pliner@gmail.com>
2023-09-28 11:54:16 +0100

    More tests

--- marshmallow_recipe/validation.py
+++ marshmallow_recipe/validation.py
@@ -7,11 +7,11 @@ import marshmallow.validate
 ValidationFunc = collections.abc.Callable[[Any], Any]
 
 
-def regexp_validate(regexp: re.Pattern | str, *, error: str | None) -> ValidationFunc:
+def regexp_validate(regexp: re.Pattern | str, *, error: str | None = None) -> ValidationFunc:
     return marshmallow.validate.Regexp(regexp, error=error)
 
 
-def validate(validator: ValidationFunc, *, error: str | None) -> ValidationFunc:
+def validate(validator: ValidationFunc, *, error: str | None = None) -> ValidationFunc:
     if error is None:
         return validator
 
