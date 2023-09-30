ba375e327e2a0a07b020821a304d6496c4290361
Yury Pliner <yury.pliner@gmail.com>
2023-09-30 11:35:40 +0100

    Add validation field errors

--- marshmallow_recipe/validation.py
+++ marshmallow_recipe/validation.py
@@ -1,4 +1,5 @@
 import collections.abc
+import dataclasses
 import re
 from typing import Any
 
@@ -22,3 +23,38 @@ def validate(validator: ValidationFunc, *, error: str | None = None) -> Validati
         return result
 
     return _validator_with_custom_error
+
+
+ValidationError = marshmallow.ValidationError
+
+
+@dataclasses.dataclass(kw_only=True, frozen=True, slots=True)
+class ValidationFieldError:
+    name: str
+    error: str | None = None
+    nested_errors: list["ValidationFieldError"] | None = None
+
+
+def get_field_errors(exc: ValidationError) -> list[ValidationFieldError]:
+    return __get_field_errors_from_normalized_messages(exc.normalized_messages())
+
+
+def __get_field_errors_from_normalized_messages(normalized_messages: dict[Any, Any]) -> list[ValidationFieldError]:
+    errors: list["ValidationFieldError"] = []
+
+    for key, value in normalized_messages.items():
+        if not isinstance(key, (str, int)):
+            continue
+
+        if isinstance(value, dict):
+            errors.append(
+                ValidationFieldError(name=str(key), nested_errors=__get_field_errors_from_normalized_messages(value))
+            )
+        elif isinstance(value, str):
+            errors.append(ValidationFieldError(name=str(key), error=value))
+        elif isinstance(value, list) and all([isinstance(item, str) for item in value]):
+            errors.append(ValidationFieldError(name=str(key), error=str.join("; ", value)))
+
+    errors.sort(key=lambda x: x.name)
+
+    return errors
