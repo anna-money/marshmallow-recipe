42224f000eab334f2f281f59626f92433f7d937a
Iurii Pliner <yury.pliner@gmail.com>
2024-01-15 09:07:13 +0000

    Revert "Refactor benchmark"
    This reverts commit fb54a52147c905fbf127e763c3ab7310c8fdacb6.

--- benchmarks/transaction_load_dump.py
+++ benchmarks/transaction_load_dump.py
@@ -2,9 +2,10 @@ import cProfile
 import dataclasses
 import datetime
 import decimal
+import marshmallow_recipe as mr
 import uuid
 
-import marshmallow_recipe as mr
+from typing import Annotated
 
 
 @dataclasses.dataclass(frozen=True)
@@ -12,8 +13,8 @@ class Transaction:
     id: uuid.UUID
     created_at: datetime.datetime
     processed_at: datetime.datetime | None
-    amount: decimal.Decimal
-    transaction_amount: decimal.Decimal
+    amount: decimal.Decimal = dataclasses.field(metadata=mr.decimal_metadata(places=4))
+    transaction_amount: Annotated[decimal.Decimal, mr.decimal_metadata(places=4)]
 
 
 transaction = Transaction(
@@ -23,10 +24,8 @@ transaction = Transaction(
     amount=decimal.Decimal(42),
     transaction_amount=decimal.Decimal(42),
  )
-transactions = [transaction] * 1024
 
+# to warm up the lib caches
+assert mr.load_many(Transaction, mr.dump_many([transaction] * 1024))
 
-assert mr.load_many(Transaction, mr.dump_many(transactions))
-assert mr.load_slim_many(Transaction, mr.dump_slim_many(transactions))
-cProfile.run("mr.load_slim_many(Transaction, mr.dump_slim_many(transactions))", sort='tottime')
-cProfile.run("mr.load_many(Transaction, mr.dump_many(transactions))", sort='tottime')
+cProfile.run("mr.load_many(Transaction, mr.dump_many([transaction] * 1024))", sort='tottime')
