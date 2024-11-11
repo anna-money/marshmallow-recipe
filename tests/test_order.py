5c05cb3277f82b07023fde2383dfdbb8ba9fe708
Iurii Pliner <yury.pliner@gmail.com>
2024-11-11 11:35:42 +0000

    One more test to demonstrate how it works

--- tests/test_order.py
+++ tests/test_order.py
@@ -3,10 +3,19 @@ import dataclasses
 import marshmallow_recipe as mr
 
 
-def test_order():
+def test_order_1():
     @dataclasses.dataclass
     class A:
         a: int
         b: int
 
     assert [name for name in mr.dump(A(a=1, b=2))] == ["a", "b"]
+
+
+def test_order_2():
+    @dataclasses.dataclass
+    class A:
+        b: int
+        a: int
+
+    assert [name for name in mr.dump(A(b=1, a=2))] == ["b", "a"]
