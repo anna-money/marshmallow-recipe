ef4e41a105c08876a9cf9d3f13c1e5f9c2fbb37b
Iurii Pliner <yury.pliner@gmail.com>
2025-04-07 10:23:25 +0100

    Make cls/data positional arguments only (#172)

--- marshmallow_recipe/serialization.py
+++ marshmallow_recipe/serialization.py
@@ -1,6 +1,6 @@
 import dataclasses
 import importlib.metadata
-from typing import Any, Protocol, TypeVar
+from typing import Any, TypeVar
 
 import marshmallow as m
 
@@ -20,39 +20,9 @@ class _SchemaKey:
 
 _schemas: dict[_SchemaKey, m.Schema] = {}
 
-
-class SchemaFunction(Protocol):
-    def __call__(self, cls: type, *, many: bool = False, naming_case: NamingCase | None = None) -> m.Schema: ...
-
-
-class LoadFunction(Protocol):
-    def __call__(self, cls: type[_T], data: dict[str, Any], *, naming_case: NamingCase | None = None) -> _T: ...
-
-
-class LoadManyFunction(Protocol):
-    def __call__(
-        self, cls: type[_T], data: list[dict[str, Any]], *, naming_case: NamingCase | None = None
-    ) -> list[_T]: ...
-
-
-class DumpFunction(Protocol):
-    def __call__(self, data: Any, *, naming_case: NamingCase | None = None) -> dict[str, Any]: ...
-
-
-class DumpManyFunction(Protocol):
-    def __call__(self, data: list[Any], *, naming_case: NamingCase | None = None) -> list[dict[str, Any]]: ...
-
-
-schema: SchemaFunction
-load: LoadFunction
-load_many: LoadManyFunction
-dump: DumpFunction
-dump_many: DumpManyFunction
-
-
 if _MARSHMALLOW_VERSION_MAJOR >= 3:
 
-    def schema_v3(cls: type, *, many: bool = False, naming_case: NamingCase | None = None) -> m.Schema:
+    def schema_v3(cls: type, /, *, many: bool = False, naming_case: NamingCase | None = None) -> m.Schema:
         key = _SchemaKey(cls=cls, many=many, naming_case=naming_case)
         existent_schema = _schemas.get(key)
         if existent_schema is not None:
@@ -63,18 +33,21 @@ if _MARSHMALLOW_VERSION_MAJOR >= 3:
 
     schema = schema_v3
 
-    def load_v3(cls: type[_T], data: dict[str, Any], *, naming_case: NamingCase | None = None) -> _T:
+    def load_v3(cls: type[_T], data: dict[str, Any], /, *, naming_case: NamingCase | None = None) -> _T:
         return schema_v3(cls, naming_case=naming_case).load(data)  # type: ignore
 
     load = load_v3
 
-    def load_many_v3(cls: type[_T], data: list[dict[str, Any]], *, naming_case: NamingCase | None = None) -> list[_T]:
+    def load_many_v3(
+        cls: type[_T], data: list[dict[str, Any]], /, *, naming_case: NamingCase | None = None
+    ) -> list[_T]:
         return schema_v3(cls, many=True, naming_case=naming_case).load(data)  # type: ignore
 
     load_many = load_many_v3
 
     def dump_v3(
         data: Any,
+        /,
         *,
         naming_case: NamingCase | None = None,
     ) -> dict[str, Any]:
@@ -86,7 +59,7 @@ if _MARSHMALLOW_VERSION_MAJOR >= 3:
 
     dump = dump_v3
 
-    def dump_many_v3(data: list[Any], *, naming_case: NamingCase | None = None) -> list[dict[str, Any]]:
+    def dump_many_v3(data: list[Any], /, *, naming_case: NamingCase | None = None) -> list[dict[str, Any]]:
         if not data:
             return []
         data_schema = schema_v3(type(data[0]), many=True, naming_case=naming_case)
@@ -99,7 +72,7 @@ if _MARSHMALLOW_VERSION_MAJOR >= 3:
 
 else:
 
-    def schema_v2(cls: type, *, many: bool = False, naming_case: NamingCase | None = None) -> m.Schema:
+    def schema_v2(cls: type, /, *, many: bool = False, naming_case: NamingCase | None = None) -> m.Schema:
         key = _SchemaKey(cls=cls, many=many, naming_case=naming_case)
         existent_schema = _schemas.get(key)
         if existent_schema is not None:
@@ -110,13 +83,15 @@ else:
 
     schema = schema_v2
 
-    def load_v2(cls: type[_T], data: dict[str, Any], *, naming_case: NamingCase | None = None) -> _T:
+    def load_v2(cls: type[_T], data: dict[str, Any], /, *, naming_case: NamingCase | None = None) -> _T:
         loaded, _ = schema_v2(cls, naming_case=naming_case).load(data)  # type: ignore
         return loaded  # type: ignore[return-value]
 
     load = load_v2
 
-    def load_many_v2(cls: type[_T], data: list[dict[str, Any]], *, naming_case: NamingCase | None = None) -> list[_T]:
+    def load_many_v2(
+        cls: type[_T], data: list[dict[str, Any]], /, *, naming_case: NamingCase | None = None
+    ) -> list[_T]:
         loaded, _ = schema_v2(cls, many=True, naming_case=naming_case).load(data)  # type: ignore
         return loaded  # type: ignore[return-value]
 
@@ -124,6 +99,7 @@ else:
 
     def dump_v2(
         data: Any,
+        /,
         *,
         naming_case: NamingCase | None = None,
     ) -> dict[str, Any]:
@@ -133,11 +109,11 @@ else:
             raise m.ValidationError(errors)
         if errors := data_schema.validate(dumped):
             raise m.ValidationError(errors)
-        return dumped
+        return dumped  # type: ignore
 
     dump = dump_v2
 
-    def dump_many_v2(data: list[Any], *, naming_case: NamingCase | None = None) -> list[dict[str, Any]]:
+    def dump_many_v2(data: list[Any], /, *, naming_case: NamingCase | None = None) -> list[dict[str, Any]]:
         if not data:
             return []
         data_schema = schema_v2(type(data[0]), many=True, naming_case=naming_case)
@@ -146,7 +122,7 @@ else:
             raise m.ValidationError(errors)
         if errors := data_schema.validate(dumped):
             raise m.ValidationError(errors)
-        return dumped
+        return dumped  # type: ignore
 
     dump_many = dump_many_v2
 
